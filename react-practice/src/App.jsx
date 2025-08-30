dc7254f5e076ffa6b580177bc245241f4cf031bb
spec3290 <spec3290@gmail.com>
2025-08-30 23:37:28 +0900

    axios 통신 연습 코드

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,29 @@
+import React, {use, useEffect, useState} from "react";
+import axios from "axios";
+
+const App = () =>{
+
+const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    // axios({
+    //   method:'GET',
+    //   url:'https://jsonplaceholder.typicode.com/photos'
+    // }).then(response => setPosts(response.data))
+    axios.get('https://jsonplaceholder.typicode.com/photos')
+        .then(response => setPosts(response.data))
+  }, [])
+
+  return(
+    <ul>
+      {posts.map(post => (
+        <li key={post.id}>
+          <div>{post.title}</div>
+          <div><img src={post.thumbnailUrl}/></div>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default App;
